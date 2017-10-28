964483ea87d46830c95b1425255911bf15d9e744
Maxwell Carlson <bart4282819@gmail.com>
2017-10-28 01:57:26 -0700

    Initial commit

--- Dwarf/Main.cpp
+++ Dwarf/Main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	TCODConsole::initRoot(80, 50, "C++ libtcod tutorial");
+	TCODConsole::initRoot(80, 50, "C++ libtcod tutorial", false);
 	TCODConsole::root->printEx(40, 25, TCOD_BKGND_NONE, TCOD_CENTER, "Hello world");
 	TCODConsole::flush();
 	TCODConsole::waitForKeypress(true);
