aaf57fa2657293d6d2ab75bff690404988b8ade2
Maxwell Carlson <bart4282819@gmail.com>
2017-11-10 00:32:03 -0800

    Figured out how to render tiles of equal dimensions

--- Dwarf/Main.cpp
+++ Dwarf/Main.cpp
@@ -17,7 +17,7 @@ Engine engine(129, 129);
 int main()
 {
 	terminal_open();
-	terminal_set("window.title='Dwarf'; window.size=155x65; window.cellsize=auto");
+	terminal_set("window.title='Dwarf'; window.size=80x80; window.cellsize=16x16");
 	terminal_set("U+E100: ../Obsidian_16x16.png, size=16x16");
 	terminal_set("U+E200: ../Spacefox_16x16Dibujor01.png, size=16x16");
 	terminal_composition(true);
