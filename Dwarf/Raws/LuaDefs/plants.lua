6948e830ecb942f3ed1f0565dc4939266fe47e1e
Maxwell Carlson <bart4282819@gmail.com>
2018-01-20 13:20:01 -0800

    Plant lua defs are now being read in properly

--- Dwarf/Raws/LuaDefs/plants.lua
+++ Dwarf/Raws/LuaDefs/plants.lua
@@ -7,15 +7,21 @@ function build_plants(name, cycles, glyphs, harvest, tags) return { name = name,
 
 function harvest_normal(h) return {"none", "none", h, h } end
 
-function veg_a(a, ac, b, bc, c, cc, d, dc) return { a = { glyphs[a], col = ac }, b = { glyphs[b], col = bc }, c = { glyphs[c], col = cc }, d = { glyphs[d], col = dc }} end
+function veg_a(a, ac, b, bc, c, cc, d, dc) return 
+{ 
+	a = { glyph = glyphs[a], col = ac }, 
+	b = { glyph = glyphs[b], col = bc }, 
+	c = { glyph = glyphs[c], col = cc },
+	d = { glyph = glyphs[d], col = dc }
+} end
 
 
-grass_glyphs = vega_a('grass', "brown", 'grass', "green",  'grass', "brown", 'grass',  "brown"  );
+grass_glyphs = veg_a('grass', "brown", 'grass', "green",  'grass', "brown", 'grass',  "brown"  );
 
---function flower_glyphs(color) return veg_a('grass', "brown", 'sprout', "green", 'flower', color, 'sprout', "yellow" ); end
+function flower_glyphs(color) return veg_a('grass', "brown", 'sprout', "green", 'flower', color, 'sprout', "yellow" ); end
 
 vegetation = {
 
 	grass = build_plants("Grass", grass_lifecycle, grass_glyphs, harvest_normal('none'), {'spread'}),
-	--tulip = build_plants("Tulip", grass_lifecycle, flower_glyphs("purple"), harvest_normal('none'), {'spread'}),
+	tulip = build_plants("Tulip", grass_lifecycle, flower_glyphs("purple"), harvest_normal('none'), {'spread'}),
 };
