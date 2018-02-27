b28ead20daf4357f9d983b9c70524b1f9e385c55
Maxwell Carlson <bart4282819@gmail.com>
2018-02-27 00:32:19 -0800

    Added more body parts. Still debating on the internal structure these will be held in so haven't made the reader for them yet

--- Dwarf/Raws/LuaDefs/body_parts.lua
+++ Dwarf/Raws/LuaDefs/body_parts.lua
@@ -3,32 +3,127 @@
 -- TODO: add effects like on % damaged, On destoryed, etc
 -- TODO: Add major groups (like head area) and add % covered to individual parts
 -- TODO: Add insides  somehow?
+-- TODO: Add in sizes e.g. if head is hit skull should have greater chance to be hit than eye, and also greater than brain!
+	-- Should they be dynamic? That will make definining species a bit of a pain
 
 body_parts = {
+
+	head = {
+		name = "Head",
+		description = "",
+		health = 35
+	},
+
+		skull = {
+			name = "Skull",
+			description = "",
+			health = 35,
+		},
+
+		brain = {
+			name = "Brain",
+			description = "",
+			health = 15,
+		},
+
+		eye = {
+			name = "Eye",
+			description = "",
+			health = 10,
+		},
+
+		ear = {
+			name = "Ear",
+			description = "",
+			health = 10,
+		},
+
+		nose = {
+			name = "Nose",
+			description = "",
+			health = 15,
+		},
+	
+		neck = {
+			name = "Neck",
+			description = "",
+			health = 20,
+		}
 
 	toroso = {
 		name = "Torso",
 		description = "",
-		health = 40,
+		health = 55,
 	},
+		arm = {
+			name = "Arm",
+			description = "",
+			health = 35,
+		},
 
-	sternum 
+		hand = {
+			name = "Hand",
+			description = "",
+			health = 15,
+		},
 
-	brain = {
-		name = "Brain",
+		finger = {
+			name = "Finger",
+			description = "",
+			health = 15,
+		},
+
+		heart = {
+			name = "Heart",
+			description = "",
+			health = 8,
+		},
+
+		lung = {
+			name = "Lung",
+			description = "",
+			health = 15,
+		},
+
+		stomach = {
+			name = "Stomach",
+			description = "",
+			health = 15,
+		},
+
+		liver = {
+			name = "Liver",
+			description = "",
+			health = 15,
+		},
+
+		kidney = {
+			name = "Kidney",
+			description = "",
+			health = 15,
+		},
+
+	waist = {
+		name = "Waist",
 		description = "",
-		health = 15,
+		health = 85,
 	},
+	
+		leg = {
+			name = "Leg",
+			description = "",
+			health = 35,
+		},
 
-	skull = {
-		name = "Skull",
-		description = "",
-		health = 35,
-	},
+		foot = {
+			name = "Foot",
+			description = "",
+			health = 20,
+		},
 
-	eye = {
-		name = "Eye",
-		description = "",
-		health = 10,
-	}
+		toe = {
+			name = "Toe",
+			description = "",
+			health = 6,
+		},
 };
--- Dwarf/Raws/LuaDefs/species.lua
+++ Dwarf/Raws/LuaDefs/species.lua
@@ -1,6 +1,31 @@
 -- Define species traits here
 
-humanParts = {"hand", "hand"}
+humanParts = 
+{
+	head = {
+		{skull = 1},
+		{brain = 1},
+		{eye = 2},
+		{ear = 2},
+		{nose = 1},
+		{neck = 1}
+	},
+	toroso = { 
+		{arm = 2},
+		{hand = 2},
+		{finger = 10},
+		{heart = 1},
+		{lung = 2},
+		{stomach = 1},
+		{liver = 1},
+		{kidney = 2}
+	},
+	waist = {
+		{leg = 2}, 
+		{foot = 2}, 
+		{toe = 10} 
+	}
+}
 
 species = {
 
